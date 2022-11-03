c623cbd4c2391b7d300d1ac1aa7784f60fefa84c
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-11-03 18:45:40 +0200

    adde AdderPlus and RCA to reports

--- Project_1/scripts/0_init_design.tcl
+++ Project_1/scripts/0_init_design.tcl
@@ -18,11 +18,13 @@ set search_path    "${demo_rtl_dir}/** ${library_path} ${all_rtl_sub_directories
 ###########################TODO Update top Module parameters#####################
 set parameters 0
 set param_values { {N 8} }
-set top_module "fp_adder"
+set top_module "simpleAdder"
 set output_dir "${ekit_dir}/${top_module}"
 
 ###########################TODO RTL and Constraints paths ######################
-set rtl_list { "fp_adder.v" "count_leading_zeros.v" "simpleAdder.v" }
+# set rtl_list { "fp_adder.v" "count_leading_zeros.v" "simpleAdder.v" }
+# set rtl_list { "ripple_carry_adder.v" "fa.v" "ha.v" }
+set rtl_list { "simpleAdder.v"}
 set demo_adder_sdc_files "${ekit_dir}/constraints/demo_adder_func.sdc"
 ################################# RTL and Libraries ###########################################
 
