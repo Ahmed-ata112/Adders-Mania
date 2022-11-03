650db3d818068aeb968efdda33e88f75843a0e78
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2022-11-03 10:48:03 +0200

    making CSA,CPS generic

--- Project_1/rtl/CarrySkipAdder/carry_skip_adder.v
+++ Project_1/rtl/CarrySkipAdder/carry_skip_adder.v
@@ -1,4 +1,4 @@
-module carrySkipAdder #(parameter N = 32)
+module CSA #(parameter N = 32)
 (
 		a,b,cin,
 		sum,cout
@@ -8,16 +8,18 @@ module carrySkipAdder #(parameter N = 32)
     	output cout;
     	output [N-1:0] sum;
 
-	wire [7:0] couts;
-	wire [6:0] temp;
+	wire [N/4-1:0] couts;
+	wire [N/4-2:0] temp;
 
 	ripple_carry_adder rc1(a[3:0],b[3:0],0,couts[0],sum[3:0]);
-	ripple_carry_adder rc[7:1](a[31:4],b[31:4],temp[6:0],couts[7:1],sum[31:4]);
+	ripple_carry_adder rc[N/4-1:1](a[N-1:4],b[N-1:4],temp[N/4-2:0],couts[N/4-1:1],sum[N-1:4]);
 
 	skipLogic skip0(a[3:0],b[3:0],0,couts[0],temp[0]);
-	skipLogic skip[6:1](a[27:4],b[27:4],temp[5:0],couts[6:1],temp[6:1]);
-	skipLogic skip7(a[31:28],b[31:28],temp[6],couts[7],cout);
+	skipLogic skip[N/4-2:1](a[N-5:4],b[N-5:4],temp[N/4-3:0],couts[N/4-2:1],temp[N/4-2:1]);
+	skipLogic skip7(a[N-1:N-4],b[N-1:N-4],temp[N/4-2],couts[N/4-1],cout);
 endmodule
+
+
 // the logic of getting the total propagation (selector of the mux)
 module skipLogic #(parameter N=4)
 (
@@ -30,20 +32,14 @@ module skipLogic #(parameter N=4)
 
 	wire [N-1:0] p;
 	wire finalP;
+	wire e;
 	
 	genvar i;
 	for(i=0; i<N; i=i+1) begin
-		assign p[i]= a[i]^b[i];
+		assign p[i]= a[i] ^ b[i];
 	end
-	assign finalP=&p;
+	assign finalP= &p;
 
-	mux21 skipMux(cout,cin,finalP,out);
-endmodule
-
-// multiplixer 2*1 to select one of (the carry of the ripple adder or the input carry)
-module mux21 (in1,in2,selector,out);
-	input in1,in2,selector;
-	output out;
-
-	assign out = selector?in2 :in1;
+	assign e= finalP & cin;
+	assign out= e | cout;
 endmodule
